078a50f9f0079bbefbec0ca95f1fda9a338ebf00
Troy Jones <sntnmjones@gmail.com>
2020-02-04 18:07:59 -0800

    added routes to express

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const app = express();
+var cors = require('cors');
 
 connectDB();
+const app = express();
+
+// enable cors
+app.use(cors({origin:true, credentials:true}));
+
+// init middleware
+// app.use(express.json({extended:false}));
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+const books = require('./routes/api/books');
+app.use('/api/books', books);
 const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server running on port ${port}`))
